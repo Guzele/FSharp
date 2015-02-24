e2e55e5be95f1e5570404e9025242eb8c41ce617
GuzelGarifullina <GiseleGarifullina@gmail.com>
2015-02-24 21:34:32 +0300

    Renamed binary search tree

---
+++ a2_binary_search_tree/a2_binary_search_tree/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module a2_binary_search_tree.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("a2_binary_search_tree")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("guzel")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
