2b2cd8a6f37da989e75c29c792f3f585d530db8f
GuzelGarifullina <GiseleGarifullina@gmail.com>
2015-03-04 01:36:57 +0300

    Filter list

--- b2_list/b2_list/Program.fs
+++ b2_list/b2_list/Program.fs
@@ -2,14 +2,26 @@
    Author Guzel Garifullina 171
    reverse
    Estimated time 30 minutes
-   real time      2 hours   *)
+   real time      2 hours
+   filter
+   Estimated time 30 minutes
+   real time      30 minutes   *)
 
 let reverse l = List.fold (fun acc  x -> x :: acc ) [] l 
 
+let filter f l =
+    let l = List.fold (fun acc x -> if f x then x :: acc
+                                    else acc) [] l  
+    reverse l
+
+let is_even x = (x % 2 = 0)
+
+
 [<EntryPoint>]
 let main argv = 
-    let l = [1; 2; 3; 4; 5; 5; 4]
+    let l = [1; 2; 3; 6; 4; 5; 5; 10; 4]
     printfn "Initial list %A"  l
     printfn "Reversed list %A" (reverse  l)
+    printfn "Filter list only even %A" ( filter (is_even ) l ) 
     0 
 
