6deae52d6fb027e2943c0b33a4c335a4aad5ecb7
GuzelGarifullina <GiseleGarifullina@gmail.com>
2015-03-04 01:06:35 +0300

    Reverse list with fold

---
+++ b2_list/b2_list/Program.fs
@@ -0,0 +1,15 @@
+﻿(* List transformation with fold - second week homework
+   Author Guzel Garifullina 171
+   reverse
+   Estimated time 30 minutes
+   real time      2 hours   *)
+
+let reverse l = List.fold (fun acc  x -> x :: acc ) [] l 
+
+[<EntryPoint>]
+let main argv = 
+    let l = [1; 2; 3; 4; 5; 5; 4]
+    printfn "Initial list %A"  l
+    printfn "Reversed list %A" (reverse  l)
+    0 
+
