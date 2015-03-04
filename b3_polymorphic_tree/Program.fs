f57ed9500671b76d3f265226d9f64f21a210a312
GuzelGarifullina <GiseleGarifullina@gmail.com>
2015-03-05 01:06:31 +0300

    Polimorphic tree type

---
+++ b3_polymorphic_tree/Program.fs
@@ -0,0 +1,14 @@
+﻿(* Polymorphic tree - second week homework
+   Author Guzel Garifullina 171
+   type Tree
+   Estimated time 15 minutes
+   real time      5 minute  *)
+
+type Tree<'A> = Nil | Node of Tree<'A> * 'A * Tree<'A>
+
+[<EntryPoint>]
+let main argv = 
+    let a = (Nil, "e", Nil)
+    printfn "%A" a
+    0 
+
