24c30ead72827b5f82afda27ff01702ce7b1ba47
GuzelGarifullina <GiseleGarifullina@gmail.com>
2015-04-12 09:06:01 +0300

    Add a little more tests

--- f_Calculator/test/IntTest.fs
+++ f_Calculator/test/IntTest.fs
@@ -7,11 +7,14 @@ open NUnit.Framework
 open Expr
 open Stmt
 open Interpreter
-
-
-
+/// read from list, check while
 [<TestCase (";\nread\nx\n;\nread\nn\n;\n:=\nres\n1\n;\nwhile\nn\n\n;\n:=\nres\n*\nres\nx\n:=\nn\n-\nn\n1\nwrite\nres\n", Result="4\n")>]
+/// negative argument, write more than 1 argument
+[<TestCase (";\n:=\nx\n5\n;\n:=\ns\n-12\n;\n:=\nt\n-\n+\nx\ns\n2\n;\nwrite\nx\n;\nwrite\ns\nwrite\nt\n", Result="5\n-12\n-9\n")>]
+/// if then changes parametr but not write
 [<TestCase (";\n:=\nx\n5\nif\nx\n:=\nl\n-12\n;\n:=\nl\n2\nwrite\nl\n", Result="")>]
+///if else with 2 actions
+[<TestCase (";\n:=\nx\n-5\nif\nx\n:=\nl\n12\n;\n:=\nl\n-2\nwrite\nl\n", Result="-2\n")>]
 let intTest str =
     (interpritate (str |> inpToStr |> makeProgramTree) [2;2] )
 
--- f_Calculator/test/Script.fsx
+++ f_Calculator/test/Script.fsx
@@ -1,5 +1,4 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
+﻿
 #load "IntTest.fs"
 
 open test
